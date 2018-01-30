971e12fc7722e6a250df684796b9450429ffc1a5
edwin rivera <waehake@gmail.com>
2018-01-30 03:02:56 +0000

    automatically backed up by learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -4,7 +4,9 @@ import React, { Component } from 'react';
 class Keypad extends Component {
   render() {
     return (
-      
+      <div>
+        <input type="password" />
+      </div>
     );
   }
 }
