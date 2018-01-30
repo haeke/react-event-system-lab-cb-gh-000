3babec4d832f93168747484dd0849c97281d55db
edwin rivera <waehake@gmail.com>
2018-01-30 03:04:14 +0000

    automatically backed up by learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -2,10 +2,13 @@
 import React, { Component } from 'react';
 
 class Keypad extends Component {
+  automatedMessage() {
+    console.log('Entering password...');
+  }
   render() {
     return (
       <div>
-        <input type="password" />
+        <input type="password" onKeyUp={this.automatedMessage} />
       </div>
     );
   }
